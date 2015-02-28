86aa59bb06cd591b1a3111ffa24ec5e5a68ac6dd
gmena <gmjun2000@yahoo.com>
2015-02-28 15:49:33 -0600

    Client added

--- client.py
+++ client.py
@@ -16,18 +16,22 @@ class WebSocketClient(WebSocketClientProtocol):
         return self
 
     def onConnect(self, response):
+        """On Connect what to do?"""
         if self.events.on_connect is not None:
             self.events.on_connect(response)
 
     def onOpen(self):
+        """On Open what to do?"""
         if self.events.on_open is not None:
             self.events.on_open(self)
 
     def onMessage(self, payload, isBinary):
+        """On Message what to do?"""
         if self.events.on_message is not None:
             self.events.on_message(payload)
 
     def onClose(self, wasClean, code, reason):
+        """On Close what to do?"""
         if self.events.on_close is not None:
             self.events.on_close(code, reason)
 
@@ -42,24 +46,45 @@ class MiddleWareSocketEvent(object):
         self.on_message = None
 
     def set_on_connect(self, callback):
+        """
+        Set callback event for connect event
+        @:param callback function
+        """
         if isfunction(callback):
             self.on_connect = callback
 
     def set_on_open(self, callback):
+        """
+        Set callback event for open event
+        @:param callback function
+        """
         if isfunction(callback):
             self.on_open = callback
 
     def set_on_clone(self, callback):
+        """
+        Set callback event for close event
+        @:param callback function
+        """
         if isfunction(callback):
             self.on_close = callback
 
     def set_on_message(self, callback):
+        """
+        Set callback event for message event
+        @:param callback function
+        """
         if isfunction(callback):
             self.on_message = callback
 
 
 class SocketMiddleWare(MiddleWareSocketEvent):
     def connect_socket(self, user, port=8000):
+        """
+        Connect the client to server
+        @:param user string
+        @:param port int
+        """
         factory = WebSocketClientFactory("ws://localhost:" + str(port) + "?user=" + user, debug=False)
         factory.protocol = WebSocketClient(self)
 
@@ -71,7 +96,7 @@ class SocketMiddleWare(MiddleWareSocketEvent):
 
 # Example
 def connect(response):
-    print "connected"
+    print "I am connected callback"
 
 
 socket = SocketMiddleWare()
